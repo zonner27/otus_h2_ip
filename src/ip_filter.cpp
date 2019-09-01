721b16638a8ebee8272cd713dd36eefbb8875e5f
zonner27 <zonner2785@gmail.com>
2019-09-01 22:56:48 +0300

    Revert "remove all files for review"
    This reverts commit 5fb821ec6742a597648d6e5c92305ad519b85a9d.

---
+++ src/ip_filter.cpp
@@ -0,0 +1,111 @@
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include "lib.h"
+
+// ("",  '.') -> [""]
+// ("11", '.') -> ["11"]
+// ("..", '.') -> ["", "", ""]
+// ("11.", '.') -> ["11", ""]
+// (".11", '.') -> ["", "11"]
+// ("11.22", '.') -> ["11", "22"]
+
+int main()
+{
+    try
+    {       
+        std::vector<ip_adress> ipv;
+
+        for(std::string line; std::getline(std::cin, line);)
+        {
+            std::vector<std::string> v = split(line, '\t');
+            ipv.push_back(ipget(split(v.at(0), '.')));
+        }
+
+        std::sort(ipv.begin(), ipv.end(),  std::greater<ip_adress>());
+        print2(ipv);
+
+        auto ip_temp = ipfilter(ipv, 1);
+        print2(ip_temp);
+
+        ip_temp = ipfilter(ipv, 46, 70);
+        print2(ip_temp);
+
+        ip_temp = ipfilter_any(ipv, 46);
+        print2(ip_temp);
+
+
+        // 222.173.235.246
+        // 222.130.177.64
+        // 222.82.198.61
+        // ...
+        // 1.70.44.170
+        // 1.29.168.152
+        // 1.1.234.8
+
+        // TODO filter by first byte and output
+        // ip = filter(1)
+
+        // 1.231.69.33
+        // 1.87.203.225
+        // 1.70.44.170
+        // 1.29.168.152
+        // 1.1.234.8
+
+        // TODO filter by first and second bytes and output
+        // ip = filter(46, 70)
+
+        // 46.70.225.39
+        // 46.70.147.26
+        // 46.70.113.73
+        // 46.70.29.76
+
+        // TODO filter by any byte and output
+        // ip = filter_any(46)
+
+        // 186.204.34.46
+        // 186.46.222.194
+        // 185.46.87.231
+        // 185.46.86.132
+        // 185.46.86.131
+        // 185.46.86.131
+        // 185.46.86.22
+        // 185.46.85.204
+        // 185.46.85.78
+        // 68.46.218.208
+        // 46.251.197.23
+        // 46.223.254.56
+        // 46.223.254.56
+        // 46.182.19.219
+        // 46.161.63.66
+        // 46.161.61.51
+        // 46.161.60.92
+        // 46.161.60.35
+        // 46.161.58.202
+        // 46.161.56.241
+        // 46.161.56.203
+        // 46.161.56.174
+        // 46.161.56.106
+        // 46.161.56.106
+        // 46.101.163.119
+        // 46.101.127.145
+        // 46.70.225.39
+        // 46.70.147.26
+        // 46.70.113.73
+        // 46.70.29.76
+        // 46.55.46.98
+        // 46.49.43.85
+        // 39.46.86.85
+        // 5.189.203.46
+    }
+    catch(const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
